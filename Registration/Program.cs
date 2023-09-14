046c754542b8929df89a085a7ee580af6f45bcf1
Pushti Prajapati <pushti.prajapati@reynasolutions.com>
2023-09-14 15:43:03 +0530

    PP - Layout

--- Registration/Program.cs
+++ Registration/Program.cs
@@ -39,6 +39,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Registration}/{action=Register}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
